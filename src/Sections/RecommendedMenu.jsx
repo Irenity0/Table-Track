b11ca30bf44bf8d4d89465bc6d7937ea10782304
Irenity <lilithkard77@gmail.com>
2025-01-06 05:25:50 +0600

    added recommended menu section | Home Page is done~

--- src/Sections/RecommendedMenu.jsx
+++ src/Sections/RecommendedMenu.jsx
@@ -3,17 +3,18 @@ import SectionTitle from "../components/SectionTitle";
 import useMenu from "../hooks/useMenu";
 
 const RecommendedMenu = () => {
-
-    const menu = useMenu();
-    console.log(menu)
+    const [menu] = useMenu();
+    console.log(menu);
 
     return (
         <>
-        <SectionTitle title={'Chef Reccomends'} subTitle={'Should Try'}></SectionTitle>
-        <br />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        
-        </div>
+            <SectionTitle title={'Chef Recommends'} subTitle={'Should Try'}></SectionTitle>
+            <br />
+            <div className="w-10/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {menu.slice(0, 4).map(item => (
+                    <FoodCard key={item._id} item={item}></FoodCard>
+                ))}
+            </div>
         </>
     );
 };
